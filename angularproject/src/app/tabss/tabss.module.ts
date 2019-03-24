bfdba9c26efe00643f0445b921d610762065294f
kirankumarreddyk <kanaparthykiran@gmail.com>
2019-03-24 18:40:20 +0530

    tabs

--- angularproject/src/app/tabss/tabss.module.ts
+++ angularproject/src/app/tabss/tabss.module.ts
@@ -3,12 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { TabssRoutingModule } from './tabss-routing.module';
 import { TabssComponent } from './tabss/tabss.component';
+import { MatTabsModule } from '@angular/material';
+import { TabsModule } from 'ngx-bootstrap'; //1
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap"; //2
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [TabssComponent],
   imports: [
     CommonModule,
-    TabssRoutingModule
+    TabssRoutingModule,
+    MatTabsModule,
+    TabsModule.forRoot(), //1
+    NgbModule, //2
   ]
 })
 export class TabssModule { }
+
+//1
+// ng add ngx - bootstrap--component tabs
+//https://valor-software.com/ngx-bootstrap/#/tabs
+//1
+
+//2
+// npm install --save @ng-bootstrap/ng-bootstrap
+//2
+
--- angularproject/src/app/tabss/tabss/tabss.component.ts
+++ angularproject/src/app/tabss/tabss/tabss.component.ts
@@ -6,8 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tabss.component.css']
 })
 export class TabssComponent implements OnInit {
-
-  constructor() { }
+  public tabs; //2
+  constructor() { 
+    //2
+    this.tabs = [
+      { title: "foo1", content: "bar1" },
+      { title: "foo2", content: "bar2" },
+      { title: "foo3", content: "bar3" }
+    ];
+    //2
+  }
 
   ngOnInit() {
   }
