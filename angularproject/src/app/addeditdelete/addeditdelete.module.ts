f8f05e362e2ed1a19ef2916864bcc7a886748048
kirankumarreddyk <kanaparthykiran@gmail.com>
2019-03-24 18:45:57 +0530

    adddelete

--- angularproject/src/app/addeditdelete/addeditdelete.module.ts
+++ angularproject/src/app/addeditdelete/addeditdelete.module.ts
@@ -3,12 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { AddeditdeleteRoutingModule } from './addeditdelete-routing.module';
 import { AddeditdeleteComponent } from './addeditdelete/addeditdelete.component';
+import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
+import { DataTableModule, ButtonModule, SharedModule } from 'primeng/primeng'; // add delete save
 
 @NgModule({
   declarations: [AddeditdeleteComponent],
   imports: [
     CommonModule,
-    AddeditdeleteRoutingModule
+    AddeditdeleteRoutingModule,
+    FormsModule, DataTableModule, ButtonModule, SharedModule
   ]
 })
 export class AddeditdeleteModule { }
+
+
+// add edit and delete
+// npm install primeng--save
+// npm install primeicons--save
